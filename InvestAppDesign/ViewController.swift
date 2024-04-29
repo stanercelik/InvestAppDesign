562a660354d2fd0ff6810509cc1c1ba016b21f5c
stanercelik <tanercelik2001@gmail.com>
2024-04-29 09:58:28 +0300

    Initial Commit

---
+++ InvestAppDesign/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InvestAppDesign
+//
+//  Created by Taner Çelik on 29.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
