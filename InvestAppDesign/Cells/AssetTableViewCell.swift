acd0176c99333208cdd6d543b470d7a4177a1c8e
stanercelik <tanercelik2001@gmail.com>
2024-05-06 17:19:50 +0300

    MyAssetPageCreated

---
+++ InvestAppDesign/Cells/AssetTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AssetTableViewCell.swift
+//  InvestAppDesign
+//
+//  Created by Taner Çelik on 6.05.2024.
+//
+
+import UIKit
+
+class AssetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ InvestAppDesign/Model/HistoryModel.swift
@@ -0,0 +1,8 @@
+//
+//  HistoryModel.swift
+//  InvestAppDesign
+//
+//  Created by Taner Çelik on 6.05.2024.
+//
+
+import Foundation
